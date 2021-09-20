45553a6e754e32582812c8ae290d3a82fbe0c38f
codeperfectplus <deepak008@live.com>
2021-09-20 12:18:43 +0530

    code refactoring

--- random_profile/__init__.py
+++ random_profile/__init__.py
@@ -17,7 +17,7 @@ street_names = ['Main', 'High', 'Pearl', 'Maple', 'Park', 'Oak', 'Pine', 'Cedar'
 fake_cities = ['Metropolis', 'Eerie', "King's Landing", 'Sunnydale', 'Bedrock', 'South Park', 'Atlantis', 'Mordor', 'Olympus', 'Dawnstar', 'Balmora', 'Gotham', 'Springfield', 'Quahog', 'Smalltown', 'Epicburg', 'Pythonville', 'Faketown', 'Westworld', 'Thundera', 'Vice City', 'Blackwater', 'Oldtown', 'Valyria', 'Winterfell', 'Braavos‎', 'Lakeview']
 states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
 
-class Name:
+class RandomProfile:
     def __init__(self,num=1):
         '''
         num = Total No. of Name You Want To Print
@@ -27,31 +27,27 @@ class Name:
         self.num = num
 
     def first_name(self):
-        #print first name
-        for i in range(self.num):
-          first = random.choice(fname)
-          print(first)
-
+        # print first name
+        first_name_list = [random.choice(fname) for _ in range(self.num)]
+        return first_name_list
+      
     def last_name(self):
         #print last name
-        for i in range(self.num):
-          last = random.choice(lname)
-          print(last)
-   
+        last_name_list = [random.choice(lname) for _ in range(self.num)]
+        return last_name_list
+
     def full_name(self):
         #print full name
-        for i in range(self.num):
-          first = random.choice(fname)
-          last = random.choice(lname)
-          print(f'{first} {last}')
+        full_name_list = [random.choice(fname) + ' ' + random.choice(lname) for _ in range(self.num)]
+        return full_name_list
 
     def full_profile(self):
         #print full profile
-        
+        profile_list = []
         for i in range(self.num):
           first = random.choice(fname)
           last = random.choice(lname)
-          phone = f'+91-{random.randint(800, 999)}{random.randint(800, 999)}{random.randint(1000,9999)}'
+          phone = f'+ +1-{random.randint(300, 600)}{random.randint(800, 999)}{random.randint(1000,9999)}'
 
           street_num = random.randint(100, 999)
           street = random.choice(street_names)
@@ -59,25 +55,15 @@ class Name:
           state = random.choice(states)
           zip_code = random.randint(10000, 99999)
           
-          address = f'{street_num} {street} St., {city} {state} {zip_code}'
+          address = f'{street_num} {street} St. {city} {state} {zip_code}'
           email = first.lower() + last.lower() + '@bogusemail.com'
-          print(f'{first} {last} \n {address}- Us\n{phone}\n{email}')
-          print('-'*30)
-        print('*'*30)
-'''
-import random_profile as rp
-name = rn.Name(num=1)
 
-num = Total No. of Name You Want To Print
-deafult is 1
-To Print More Than one Name Change value of num
-
-# For First Name
-name.first_name()
-
-# For First Name
-name.full_name()
-
-# For First Name
-name.full_profile()
-'''
+          profile_dict = {}
+          profile_dict['first_name'] = first
+          profile_dict['last_name'] = last
+          profile_dict['phone'] = phone
+          profile_dict['address'] = address
+          profile_dict['email'] = email
+          profile_list.append(profile_dict)
+    
+        return profile_list
--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     name="random_profile",
  
     #version of the module
-    version="0.0.5",
+    version="0.0.8",
  
     #Name of Author
     author="CodePerfectPlus",
